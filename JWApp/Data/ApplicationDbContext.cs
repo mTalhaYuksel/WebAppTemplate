cc344165effaea844c45107efa51ed2d7c394f4a
limonade <talhayuksel432@gmail.com>
2026-02-09 14:19:22 +0300

    Add project files.

---
+++ JWApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JWApp.Models;
+
+namespace JWApp.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+        public DbSet<JWApp.Models.Joke> Joke { get; set; } = default!;
+    }
+}
---
+++ JWApp/Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿namespace JWApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+
+        public String? JokeQuestion { get; set; }
+        public String? JokeAnswer { get; set; }
+
+        public Joke()
+        {
+                
+        }
+    }
+}
